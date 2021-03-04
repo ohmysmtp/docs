026d80cbb1089c33c9e2a88f410eb841f929bca4
Paul Oms <support@ohmysmtp.com>
2021-03-04 15:29:23 +0000

    Updated sidebar

--- sidebars.js
+++ sidebars.js
@@ -1,18 +1,18 @@
 module.exports = {
   overview: {
     Overview: ['overview/introduction', 'overview/firstEmail'],
-    "HTTP API": [
-      'reference/overview',
-      'reference/authentication',
-      'reference/headers',
-      'reference/send',
-      'reference/responses'
+    Integrations: [
+      { "HTTP API": [
+        'reference/overview',
+        'reference/authentication',
+        'reference/headers',
+        'reference/send',
+        'reference/responses'
+      ] },
+      { "Ruby on Rails": ['integrations/rails'] },
+      { NodeJS: require('./typedoc-sidebar.js') },
+      { "SMTP Server": ['integrations/smtp'] },
     ],
-    // "User Guide": ['guide/bounces', 'guide/blocklist', 'guide/verification'],
-    "Ruby on Rails": ['integrations/rails'],
-    NodeJS: require('./typedoc-sidebar.js'),
-    "SMTP Server": ['integrations/smtp'],
     Guide: ['guide/lifecycle', 'guide/blocklist'],
-
   }
 };
